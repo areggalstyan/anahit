b0b62b332a0cea1a7bc978b35522fbfde58aabe5
areggalstyan <areg.galstyan.levon@gmail.com>
2021-12-28 14:37:13 +0400

    Add icon to big calendar

--- src/lib/InfoPage.svelte
+++ src/lib/InfoPage.svelte
@@ -26,6 +26,7 @@
     </Articles>
     {#if current && bigCalendar}
       <TextBar alignment="left">
+        <i class="fas fa-music text-3xl text-white mr-3" />
         <p class="mt-2 border-[1px] border-secondary rounded p-3 pl-5 pr-5">
           <Format bind:text={current} />
         </p>
