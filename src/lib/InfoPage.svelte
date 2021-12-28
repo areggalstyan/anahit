9d38da45b7523352fd2aca33edb0c4b8755b3a76
areggalstyan <areg.galstyan.levon@gmail.com>
2021-12-28 14:28:21 +0400

    Fix margin top again

--- src/lib/InfoPage.svelte
+++ src/lib/InfoPage.svelte
@@ -26,7 +26,7 @@
     </Articles>
     {#if current && bigCalendar}
       <TextBar alignment="left">
-        <p class="mt-7 border-[1px] border-secondary rounded p-3 pl-5 pr-5">
+        <p class="mt-2 border-[1px] border-secondary rounded p-3 pl-5 pr-5">
           <Format bind:text={current} />
         </p>
       </TextBar>
