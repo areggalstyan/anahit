fe602bbb7c1cb2d7a75e0d722500c3a1c6eb703e
areggalstyan <areg.galstyan.levon@gmail.com>
2021-12-30 16:00:33 +0400

    Change quality back to 60% and make onMount browser only

--- src/lib/Slider.svelte
+++ src/lib/Slider.svelte
@@ -1,18 +1,21 @@
 <script>
   import { onMount } from 'svelte';
+  import { browser } from '$app/env';
 
   export let slides;
 
   onMount(() => {
-    jQuery('.slider').slick({
-      infinite: true,
-      speed: 300,
-      variableWidth: true,
-      prevArrow: '<button class="cursor-pointer"><i class="fas fa-chevron-left text-xl ' +
-        'text-white absolute top-1/2 z-10 transform translate-x-1/2 -translate-y-1/2 left-0"></i></button>',
-      nextArrow: '<button class="cursor-pointer"><i class="fas fa-chevron-right text-xl ' +
-        'text-white absolute top-1/2 z-10 transform -translate-x-1/2 -translate-y-1/2 right-0"></i></button>'
-    });
+    if (browser) {
+      jQuery('.slider').slick({
+        infinite: true,
+        speed: 300,
+        variableWidth: true,
+        prevArrow: '<button class="cursor-pointer"><i class="fas fa-chevron-left text-xl ' +
+          'text-white absolute top-1/2 z-10 transform translate-x-1/2 -translate-y-1/2 left-0"></i></button>',
+        nextArrow: '<button class="cursor-pointer"><i class="fas fa-chevron-right text-xl ' +
+          'text-white absolute top-1/2 z-10 transform -translate-x-1/2 -translate-y-1/2 right-0"></i></button>'
+      });
+    }
   });
 </script>
 
