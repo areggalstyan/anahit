0690e0796bc3fe05523c47d62979e054f039d1e0
areggalstyan <areg.galstyan.levon@gmail.com>
2023-09-16 23:10:41 +0400

    Add teaser anchor

--- src/lib/VideoBar.svelte
+++ src/lib/VideoBar.svelte
@@ -3,7 +3,7 @@
   export let videos;
 </script>
 
-<div class="bg-primary xl:flex xl:justify-around p-5">
+<div class="bg-primary xl:flex xl:justify-around p-5" id="teaser">
   {#each videos as src, i}
     <div class="w-full flex justify-center xl:mb-0" class:mb-5={i !== videos.length - 1}>
       <Video {src} />
